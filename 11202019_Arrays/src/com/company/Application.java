12ddc37e5d89a90d540d903773907dce5fef9b63
CharlieTono <arina.ershova90@gmail.com>
2019-11-21 12:57:23 +0100

    some additional tasks

--- 11202019_Arrays/src/com/company/Application.java
+++ 11202019_Arrays/src/com/company/Application.java
@@ -6,21 +6,24 @@ public class Application {
 
     public static void main(String[] args) {
 
-        printRangeDown(100,10,10);
+        printRangeDown(100, 10, 10);
 
-        int resultA = countArithmeticProgressionUp(1,87,1);
+        int resultA = countArithmeticProgressionUp(1, 87, 1);
         System.out.println(resultA);
 
         int resultB = countArithmeticProgressionUp(5, 86, 3);
         System.out.println(resultB);
 
-        int [] array = {1,2,3,4,5,6,7,8,9,10};
+        int[] array = {1, 2, 3, 4, -5};
         reverseArray(array);
         System.out.println(Arrays.toString(array));
-
+        System.out.println(sumEvenNumbers(array));
+        System.out.println(containsOdd(array));
+        System.out.println(Arrays.toString(countPositiveNumber(array)));
+        printNumbers();
     }
 
-    public static void printRangeDown (int first, int last, int step) {
+    public static void printRangeDown(int first, int last, int step) {
 
         int number = first;
         while (number >= last) {
@@ -30,7 +33,7 @@ public class Application {
         System.out.println();
     }
 
-    public static int countArithmeticProgressionUp (int first, int last, int step) {
+    public static int countArithmeticProgressionUp(int first, int last, int step) {
 
         int sum = 0;
         int number = first;
@@ -42,12 +45,54 @@ public class Application {
         return sum;
     }
 
-    public static void reverseArray (int [] array) {
+    public static void reverseArray(int[] array) {
 
-        for (int i = 0; i < array.length/2; i++) {
-            int temp = array [i];
-            array[i] = array [array.length-i-1];
-            array[array.length-i-1] = temp;
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
         }
     }
+
+    public static boolean containsOdd(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int sumEvenNumbers(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                sum += array[i];
+            }
+        }
+        return sum;
+    }
+
+    public static int[] countPositiveNumber(int[] array) {
+        int[] newArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                newArray[i] = array[i] * 3;
+            } else {
+                newArray[i] = array[i];
+            }
+        }
+        return newArray;
+    }
+
+    public static void printNumbers() {
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 5 == 0 || i % 7 == 0 || i % 13 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+
 }
