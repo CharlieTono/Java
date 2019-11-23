859fc404d5d0b4901324ea31a64994ec0f42fe4c
CharlieTono <arina.ershova90@gmail.com>
2019-11-23 16:50:04 +0100

    solution for 0-credit & %13 issues

--- 11232019_Credits/src/Application.java
+++ 11232019_Credits/src/Application.java
@@ -2,15 +2,15 @@ public class Application {
 
     public static void main(String[] args) {
 
-        int credit = 14;
+        int credit = 26;
         System.out.println(countNumberOfCredits(credit));
     }
 
     public static int countNumberOfCredits (int creditNumber) {
         int counter = 0;
 
-        for (int i = 0; i <= creditNumber; i++) {
-            if (creditNumber != 13 && i != 13) {
+        for (int i = 1; i <= creditNumber; i++) {
+            if (i%13 != 0 && i != 13) {
                 if (creditNumber % 3 == 0) {
                     System.out.println("The credit # " + i + " is related to Mary");
                     counter++;
