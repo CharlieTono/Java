2150f5f63ce379c3aaa606a1327fb0711fcdd3cd
CharlieTono <arina.ershova90@gmail.com>
2019-11-25 11:18:00 +0100

    additional solution

--- 11232019_Credits/src/Application.java
+++ 11232019_Credits/src/Application.java
@@ -6,24 +6,19 @@ public class Application {
         System.out.println(countNumberOfCredits(credit));
     }
 
-    public static int countNumberOfCredits (int creditNumber) {
+    public static int countNumberOfCredits(int creditNumber) {
         int counter = 0;
 
         for (int i = 1; i <= creditNumber; i++) {
-            if (i%13 != 0) {
-                if (i % 3 == 0 ) {
-                    System.out.println("The credit # " + i + " is related to Mary");
-                    counter++;
-                }
-                if (i % 3 == 1) {
-                    System.out.println("The credit # " + i + " is related to Petr");
-                    counter++;
-                }
-                if (i % 3 == 2) {
-                    System.out.println("The credit # " + i + " is related to Vasily");
-                    counter++;
-                }
-            }
+            if (i % 13 == 0)
+                continue;
+            if (i % 3 == 0)
+                System.out.println("The credit # " + i + " is related to Mary");
+            else if (i % 3 == 1)
+                System.out.println("The credit # " + i + " is related to Petr");
+            else
+                System.out.println("The credit # " + i + " is related to Vasily");
+            counter++;
         }
         return counter;
     }
