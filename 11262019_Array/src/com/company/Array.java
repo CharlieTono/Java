9e7bad946f0df1488e53a93cc7a36b6129692399
CharlieTono <arina.ershova90@gmail.com>
2019-11-26 17:41:30 +0100

    creates Class ArrayUtils with methods

---
+++ 11262019_Array/src/com/company/Array.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Array {
+
+    int [] source;
+
+    public Array () {
+
+    }
+
+    public Array (int [] source) {
+
+    }
+
+    public void set (int index, int value) {
+
+    }
+
+    public int get (int index) {
+
+        return index;
+    }
+
+    public int size () {
+
+        return 0;
+    }
+
+    /**
+     * adds the value to the end of the current object
+     * @param value element to add
+     */
+
+    public static void append (int value) {
+
+    }
+
+    public String toString () {
+
+        return "";
+    }
+}
---
+++ 11262019_Array/src/com/company/ArrayUtils.java
@@ -0,0 +1,69 @@
+package com.company;
+
+public class ArrayUtils {
+
+    public static int[] copyArray(int[] array) {
+        int[] newArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+        return newArray;
+    }
+
+    public static int[] sortArray(int[] array) { //bubble sort
+
+        int temp;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static int binarySearch(int[] array, int number) {
+
+        int left = 0;
+        int right = array.length - 1;
+
+        do {
+            int mid = (left + right) / 2;
+            if (array[mid] > number) {
+                right = mid - 1;
+            } else if (array[mid] < number){
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        } while (left <= right);
+
+        return -1;
+    }
+
+    public static int[] append(int[] array, int value) {  // return copy of array with a new element in the end
+        int[] newArray = new int[array.length + 1];
+        for (int i = 0; i < array.length; i++) {
+            newArray[i]= array[i];
+        }
+        newArray[newArray.length - 1] = value;
+        return newArray;
+    }
+
+    public static int[] insert(int[] array, int index, int value) {
+
+        int[] newArray = append(array, value);
+
+        for (int i = index; i < newArray.length; i++) {
+            int temp = newArray[i];
+            newArray[i] = newArray[newArray.length-1];
+            newArray[newArray.length-1] = temp;
+        }
+        return newArray;
+    }
+}
