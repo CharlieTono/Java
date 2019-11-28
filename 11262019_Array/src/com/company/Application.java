a07ce1a73403ef817269108a063af2bfc887082b
CharlieTono <arina.ershova90@gmail.com>
2019-11-28 14:33:28 +0100

    adds Class Array with methods

--- 11262019_Array/src/com/company/Application.java
+++ 11262019_Array/src/com/company/Application.java
@@ -1,25 +1,13 @@
 package com.company;
 
-import java.util.Arrays;
-
-import static com.company.ArrayUtils.*;
-
 public class Application {
 
     public static void main(String[] args) {
 
-        int [] javaArray = new int [] {18, 1, 10, 6, 2, 4, 7};
-        Array ourArray = new Array(new int [] {1, 3, -8, 5});
+        int[] javaArray = new int[]{18, 1, 10, 6, 2, 4, 7};
 
-        int a1 = javaArray[0];
-        int a2 = ourArray.get(0);
-
-        System.out.println(Arrays.toString(copyArray(javaArray)));
-        ArrayUtils.selectionSort(javaArray);
-        System.out.println(Arrays.toString(javaArray));
-        System.out.println(binarySearch(javaArray, -3));
-        System.out.println(Arrays.toString(append(javaArray, 9)));
-        System.out.println(Arrays.toString(insert(javaArray, 3, 5)));
+        Array ourArray = new Array(javaArray);
+        System.out.println(ourArray.toString());
 
     }
 }
