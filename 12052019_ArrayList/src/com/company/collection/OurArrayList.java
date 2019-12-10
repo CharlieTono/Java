0e5576144fdd05b790fc6ade0335018a9178255c
CharlieTono <arina.ershova90@gmail.com>
2019-12-10 16:29:27 +0100

    adds methods sort into OurArrayList

--- 12052019_ArrayList/src/com/company/collection/OurArrayList.java
+++ 12052019_ArrayList/src/com/company/collection/OurArrayList.java
@@ -109,8 +109,8 @@ public class OurArrayList implements List {
 
         Object temp = source[0];
 
-        for (int i = 0; i < source.length; i++) {
-            for (int j = 0; j < source.length - i - 1; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
                 if (comparator.compare(source[j], source[j + 1]) > 0) {
                     temp = source[j];
                     source[j] = source[j + 1];
