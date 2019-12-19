3c495d1b6d3b92ca470dd04615495f6da428c4d0
CharlieTono <arina.ershova90@gmail.com>
2019-12-19 11:37:45 +0100

    updates min/max rule

--- 12172019_Random/src/com/company/MinMaxRule.java
+++ 12172019_Random/src/com/company/MinMaxRule.java
@@ -4,19 +4,19 @@ import java.util.Random;
 
 public class MinMaxRule implements RandomRule {
 
+    Random random = new Random();
+
     int max;
     int min;
 
     public MinMaxRule(int min, int max) {
-        this.max = max;
         this.min = min;
+        this.max = max;
     }
 
     @Override
     public int nextInt() {
-        Random random = new Random();
-        int number = min + random.nextInt(max - min);
-        return number;
+        return (int) (min + Math.abs(random.nextLong()) % ((long) max - min));
     }
 
 }
