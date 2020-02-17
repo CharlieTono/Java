1417efe782c8c3de7a4bb78f724e0097ea65a741
CharlieTono <arina.ershova90@gmail.com>
2020-02-17 11:24:25 +0100

    add IncrementingProject

---
+++ 02172020_Incrementer/src/com/company/Incrementer.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Incrementer {
+
+    private int counter;
+
+    public void increment() {
+        counter++;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+}
