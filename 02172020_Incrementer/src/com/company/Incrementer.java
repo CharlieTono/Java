7cb85313d0ee5c7f9b8c84f7098f7a198fe31fbe
CharlieTono <arina.ershova90@gmail.com>
2020-02-17 12:27:09 +0100

    adds documentation

--- 02172020_Incrementer/src/com/company/Incrementer.java
+++ 02172020_Incrementer/src/com/company/Incrementer.java
@@ -3,10 +3,30 @@ package com.company;
 public class Incrementer {
 
     private int counter;
+//    Object mutex = new Object();
 
     public synchronized void increment() {
         counter++;
     }
+
+//    the same as:
+//    public void increment() {
+//        synchronized (this) {
+//            counter++;
+//        }
+//    }
+
+//    public void increment() { mutex - if it is not necessary to synchronized full method
+//        synchronized (mutex) {
+//            counter++;
+//        }
+//    }
+
+//    public void decrement () { mutex can close all the methods/loops, if it mention in the code
+//        synchronized (mutex) { if mutex is free, thread can use it (but only one thread), for other threads it will be closed
+//            counter--;
+//        }
+//    }
 
     public int getCounter() {
         return counter;
