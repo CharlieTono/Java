f44f9b9517afc65cbc64957958ca6c67d9170373
CharlieTono <arina.ershova90@gmail.com>
2020-03-05 16:11:58 +0100

    adds Employee Project

--- 03052020_StreamTasks/src/com/company/Application.java
+++ 03052020_StreamTasks/src/com/company/Application.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -31,12 +32,35 @@ public class Application {
                 .sum();
 
         System.out.println(res);
-        
-        long res02 = accountStream.filter(acc -> acc.getBalance() > 0)
-                .flatMap(account -> account.getTransactions().stream())
-                .filter(transaction -> transaction.getState() == State.CANCELED)
-                .collect(Collectors.reducing(0L, Transaction:: getSum, (currentResult, currentElt) -> currentElt+currentResult));
 
-        System.out.println(res02);
+//        long res02 = accountStream.filter(acc -> acc.getBalance() > 0)
+//                .flatMap(account -> account.getTransactions().stream())
+//                .filter(transaction -> transaction.getState() == State.CANCELED)
+//                .collect(Collectors.reducing(0L, Transaction::getSum, (currentResult, currentElt) -> currentElt + currentResult));
+//
+//        System.out.println(res02);
+
+        Employee per01 = new Employee("Alex", 2000);
+        Employee per02 = new Employee("Bob", 3000);
+        Employee per03 = new Employee("Carl", 4000);
+        Employee per04 = new Employee("David", 5000);
+        Employee per05 = new Employee("Emma", 6000);
+
+        List<Employee> col01 = Arrays.asList(per01, per02);
+        List<Employee> col02 = Arrays.asList(per03, per04);
+        List<Employee> col03 = Arrays.asList(per05);
+
+        Department dep01 = new Department("Logistic", "111-222", col01);
+        Department dep02 = new Department("Sales", "111-333", col02);
+        Department dep03 = new Department("Management", "222-111", col03);
+
+        Stream<Department> companyStream = Stream.of(dep01, dep02, dep03);
+
+        long qtyOfEmployees = companyStream.filter(dep -> dep.getCode().startsWith("111"))
+                .flatMap(dep -> dep.getEmployees().stream())
+                .filter(employee -> employee.getSalary() >= 3000)
+                .count();
+        System.out.println(qtyOfEmployees);
+
     }
 }
