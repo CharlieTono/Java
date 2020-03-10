6344924e48172f319004052eb483345d2bbf58f4
CharlieTono <arina.ershova90@gmail.com>
2020-03-10 13:11:49 +0100

    adds EnumProject

---
+++ 03102020_EnumProject/src/com/company/Transaction.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.time.ZonedDateTime;
+
+public class Transaction {
+
+    private TransactionState state;
+    private long sum;
+    private ZonedDateTime time;
+
+    public TransactionState getState() {
+        return state;
+    }
+
+    public long getSum() {
+        return sum;
+    }
+
+    public ZonedDateTime getTime() {
+        return time;
+    }
+
+    public String getStateName() {
+        return state.toString();
+    }
+}
