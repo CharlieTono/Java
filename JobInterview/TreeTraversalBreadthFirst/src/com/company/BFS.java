aa0efed826c8552d3a8818dce0f684e118e36b21
CharlieTono <arina.ershova90@gmail.com>
2020-03-11 17:15:07 +0100

    updates BFS in according to Graph

--- JobInterview/TreeTraversalBreadthFirst/src/com/company/BFS.java
+++ JobInterview/TreeTraversalBreadthFirst/src/com/company/BFS.java
@@ -1,28 +1,42 @@
 package com.company;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 public class BFS {
 
     private Deque<Integer> ourDeque;
-    private Graph graph;
+    private ArrayList<ArrayList<Integer>> ourList;
     private Integer[] dist;
 
-    public BFS(Graph graph) {
-        this.ourDeque = new ArrayDeque<>();
+    public BFS(ArrayList<ArrayList<Integer>> ourList) {
+        this.ourList = ourList;
+        ourDeque = new ArrayDeque<>();
+        dist = new Integer[]{0, 0, 0, 0, 0, 0, 0, 0};
     }
 
-    public void breadthFirst(int root) {
+    public int breadthFirst(int root, int endPoint) {
+
+        Set<Integer> result = new HashSet<>();
         ourDeque.addLast(root);
         while (ourDeque.size() > 0) {
             int currentVertex = ourDeque.removeFirst();
-            for (int childVertex : graph.getNeighborhoods(new Node (currentVertex))) {
-                if (dist[childVertex] == 0) {
-                    dist[childVertex] = 1 + dist[currentVertex];
-                    ourDeque.add(childVertex);
+            if (ourList.get(0).get(1) == endPoint && currentVertex == root) {
+                return 1;
+            }
+            for (int i = 0; i < ourList.size(); i++) {
+                for (int j = 0; j < ourList.get(i).size(); j++) {
+                    int childVertex = ourList.get(i).get(j);
+                    if (dist[childVertex] == 0) {
+                        dist[childVertex] = 1 + dist[currentVertex];
+                        ourDeque.add(childVertex);
+                        result.add(childVertex);
+                        if (childVertex == endPoint) {
+                            return result.size() - 2;
+                        }
+                    }
                 }
             }
         }
+        return 0;
     }
 }
