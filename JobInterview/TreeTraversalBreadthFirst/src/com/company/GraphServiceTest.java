eb3018ec78261d7ab9dd937afeced076016f33b9
CharlieTono <arina.ershova90@gmail.com>
2020-03-12 10:12:51 +0100

    updates

--- JobInterview/TreeTraversalBreadthFirst/src/com/company/GraphServiceTest.java
+++ JobInterview/TreeTraversalBreadthFirst/src/com/company/GraphServiceTest.java
@@ -18,7 +18,7 @@ public class GraphServiceTest {
                 Arrays.asList(1, 3, 5, 6),
                 Arrays.asList(2, 4),
                 Arrays.asList(1, 3),
-                Arrays.asList(2, 6), 
+                Arrays.asList(2, 6),
                 Arrays.asList(2, 5)
         );
         assertEquals(0, graphService.getDistance(graph, 0, 0));
