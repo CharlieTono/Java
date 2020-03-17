955fd9a2052a4519588a57eab69e4d47d38bad35
CharlieTono <arina.ershova90@gmail.com>
2020-03-17 10:27:18 +0100

    updates

--- 03162020_TCP_IP/src/com/company/Server.java
+++ 03162020_TCP_IP/src/com/company/Server.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,7 +14,7 @@ public class Server {
         Socket socket = serverSocket.accept(); // connection with port - program is going to process only after call
 
         BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream())); //reading
-        PrintWriter pw = new PrintWriter(socket.getOutputStream()); //answering
+        PrintStream pw = new PrintStream(socket.getOutputStream()); //answering
 
         String line;
         while ((line = br.readLine()) != null) { //till the end of the data
