cd8007569b52743dda037adf245e33c2229b33e0
CharlieTono <arina.ershova90@gmail.com>
2020-03-20 11:18:14 +0100

    updates server

--- 03192020_UDP_Server/src/com/company/ServerService.java
+++ 03192020_UDP_Server/src/com/company/ServerService.java
@@ -6,36 +6,29 @@ import java.net.DatagramSocket;
 
 public class ServerService implements Runnable {
 
-    static final int INCOMING_DATAGRAM_SIZE = 1024;
     private DatagramSocket datagramSocket;
+    private DatagramPacket incomingPacket;
 
-    public ServerService(DatagramSocket datagramSocket) {
+    public ServerService(DatagramSocket datagramSocket, DatagramPacket incomingPacket) {
         this.datagramSocket = datagramSocket;
+        this.incomingPacket = incomingPacket;
     }
 
     @Override
     public void run() {
+        try {
+            String line = new String(incomingPacket.getData(), 0, incomingPacket.getLength());
+            line = "Hello from UDP server: " + line;
 
-        while (true) {
-            byte[] incomingData = new byte[INCOMING_DATAGRAM_SIZE];
-            DatagramPacket incomingPacket = new DatagramPacket(incomingData, INCOMING_DATAGRAM_SIZE);
-            try {
-                datagramSocket.receive(incomingPacket);
+            DatagramPacket outgoingPacket = new DatagramPacket(
+                    line.getBytes(),
+                    line.length(),
+                    incomingPacket.getAddress(),
+                    incomingPacket.getPort());
 
-                String line = new String(incomingData, 0, incomingPacket.getLength());
-                line = "Hello from UDP server: " + line;
-
-                DatagramPacket outgoingPacket = new DatagramPacket(
-                        line.getBytes(),
-                        line.length(),
-                        incomingPacket.getAddress(),
-                        incomingPacket.getPort());
-                datagramSocket.send(outgoingPacket);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            datagramSocket.send(outgoingPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 }
